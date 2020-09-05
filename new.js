8ede7bed1d573b9fd4ff52e3c57df7150c2fdd87
balogunjamiu49@gmail.com <balogunjamiu49@gmail.com>
2020-09-05 20:18:42 +0100

    first commit

--- new.js
+++ new.js
@@ -26,3 +26,74 @@ window.onload = function(){
 		// connectParticles: true
 	})
 }
+
+
+
+
+// var current = null;
+// document.querySelector('#name').addEventListener('focus', function(e) {
+//   if (current) current.pause();
+//   current = anime({
+//     targets: 'path',
+//     strokeDashoffset: {
+//       value: 0,
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     },
+//     strokeDasharray: {
+//       value: '240 1386',
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     }
+//   });
+// });
+
+// document.querySelector('#email').addEventListener('focus', function(e) {
+//   if (current) current.pause();
+//   current = anime({
+//     targets: 'path',
+//     strokeDashoffset: {
+//       value: 0,
+//       duration: 10,
+//       easing: 'easeOutQuart'
+//     },
+//     strokeDasharray: {
+//       value: '240 1386',
+//       duration: -100,
+//       easing: 'easeOutQuart'
+//     }
+//   });
+// });
+
+// document.querySelector('#field').addEventListener('focus', function(e) {
+//   if (current) current.pause();
+//   current = anime({
+//     targets: 'path',
+//     strokeDashoffset: {
+//       value: -336,
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     },
+//     strokeDasharray: {
+//       value: '240 1386',
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     }
+//   });
+// });
+// document.querySelector('#submit').addEventListener('focus', function(e) {
+//   if (current) current.pause();
+//   current = anime({
+//     targets: 'path',
+//     strokeDashoffset: {
+//       value: -730,
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     },
+//     strokeDasharray: {
+//       value: '530 1386',
+//       duration: 700,
+//       easing: 'easeOutQuart'
+//     }
+//   });
+// });
